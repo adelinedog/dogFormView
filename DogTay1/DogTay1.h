77fd1b2e2b54dffa68f8d61b2ee1354f1772cb9c
Adeline <t_rainbow_8@hotmail.com>
2022-06-09 17:39:54 +0800

    Initial Commit

---
+++ DogTay1/DogTay1.h
@@ -0,0 +1,18 @@
+//
+//  DogTay1.h
+//  DogTay1
+//
+//  Created by Ann Mei Tay on 09/06/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DogTay1.
+FOUNDATION_EXPORT double DogTay1VersionNumber;
+
+//! Project version string for DogTay1.
+FOUNDATION_EXPORT const unsigned char DogTay1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DogTay1/PublicHeader.h>
+
+
